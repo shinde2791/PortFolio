c93a69d3dc3bccf267b1ae37344027b4fbea8ed6
jayesh.shinde <jayeshshinde2791@gmail.com>
2024-07-31 22:41:40 +0530

    portfolio

---
+++ jayesh-portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Java Developer", "Web Developer","AWS solution architect associate","DevOps Engineer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
